6efd16b57ccbc1d930cb293f2a2891023277c480
Marco Braga <maboa17@gmail.com>
2023-10-09 15:44:42 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Novo evento na Escola de Jujutsu",
+    "description": "A Escola de Jujutsu está promovendo um evento de jogos de magia entre os alunos na cidade de Tokyo.",
+    "photoCover": "https://th.bing.com/th/id/OIP.p9VtyGVQ9W7cfNGVGDrn9QHaDt?w=312&h=175&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+  },
+  {
+    "id": "2",
+    "title": "Yuji Itadori foi possuído pelo Sukuna?",
+    "description": "Na verdade, isso se trata de um acordo condicional entre Yuji Itadori e Sukuna. Yuji estava correndo perigo de vida e precisava da ajuda de Sukuna...",
+    "photoCover": "https://th.bing.com/th/id/OIP.GPw7Lm73QuMYWPn5hgnpzwHaEK?w=310&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+  },
+
+]
